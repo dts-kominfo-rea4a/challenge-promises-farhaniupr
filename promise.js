43933c0ec4b3f6316420f65ceac4129ecc73054d
farhaniupr <farhaniupr@gmail.com>
2022-09-15 22:59:14 +0700

    done challenge REA4 - Farhani

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(input)=>{
+  try {
+    var count = 0;
+
+    const count_1 = await promiseTheaterIXX()
+    for (let i=0; i<count_1.length; i++){
+      if (count_1[i].hasil == input){
+          count++;
+      }     
+    }  
+
+    const count_2 = await promiseTheaterVGC()
+    for (let i=0; i<count_2.length; i++){
+      if (count_2[i].hasil == input){
+          count++;
+      }     
+    }  
+    
+    return count;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
   promiseOutput,
